15a594114f3692eca41d40e1f3a9fca2fbe08448
vanisimov4 <144102084+vanisimov4@users.noreply.github.com>
2025-06-10 16:58:49 +0300

    App

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchContacts } from "../../redux/contacts/operations";
+import Layout from "../Layout/Layout";
 import ContactForm from "../contactForm/ContactForm";
 import SearchBox from "../searchBox/SearchBox";
 import ContactList from "../contactList/ContactList";
@@ -12,6 +13,15 @@ import {
   selectError,
 } from "../../redux/contacts/selectors";
 import "./App.css";
+
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage"));
+const RegistrationPage = lazy(() =>
+  import("../../pages/RegistrationPage/RegistrationPage")
+);
+const LoginPage = lazy(() => import("../../pages/LoginPage/LoginPage"));
+const ContactsPage = lazy(() =>
+  import("../../pages/ContactsPage/ContactsPage")
+);
 
 function App() {
   const loading = useSelector(selectLoading);
@@ -24,19 +34,24 @@ function App() {
     dispatch(fetchContacts());
   }, [dispatch]);
   return (
-    <div className="mainWrapper">
-      <h1>Phonebook</h1>
-      <Suspense fallback={null}>
-        <Routes>
-          <Route path="/" element={ }/>
-          <ContactForm />
-          <SearchBox />
-          {loading && <p>Loading contacts...</p>}
-          {error && <p>{error}</p>}
-          {contacts.length && <ContactList />}
-        </Routes>
-      </Suspense>
-    </div>
+    <Layout>
+      <div className="mainWrapper">
+        <h1>Phonebook</h1>
+        <Suspense fallback={null}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/contacts" element={<ContactsPage />} />
+            <ContactForm />
+            <SearchBox />
+            {loading && <p>Loading contacts...</p>}
+            {error && <p>{error}</p>}
+            {contacts.length && <ContactList />}
+          </Routes>
+        </Suspense>
+      </div>
+    </Layout>
   );
 }
 
